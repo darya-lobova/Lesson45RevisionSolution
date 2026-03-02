cde26f6745f6351865631b7209d12a2524a7508e
st <st@A510PC3>
2026-03-02 18:28:56 +0300

    Task03 refactor

--- Task03Project/logic.cpp
+++ Task03Project/logic.cpp
@@ -11,18 +11,17 @@ bool is_the_same_elements(int array[DEFAULT_SIZE], int size) {
 
 	bool result = false;
 
-	for (int i = 0; i < size; i++) {
-		for (int i = 0; i < size; i++)
-		{
-			for (int j = i + 1; j < size; j++)
+	for (int i = 0; i < size - 1; i++)
+	{
+		for (int j = i + 1; j < size; j++)
 
-				if (array[i] == array[j]) {
-					result = true;
-					break;
-				}
-		}
-
+			if (array[i] == array[j]) {
+				result = true;
+				break;
+			}
 	}
+
+
 
 
 	return result;
