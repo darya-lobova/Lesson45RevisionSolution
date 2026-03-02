091b57d2b763497e87999a3255c3d787ca363291
st <st@A510PC3>
2026-03-02 17:54:42 +0300

    Task04 solved

--- Task04Project/logic.cpp
+++ Task04Project/logic.cpp
@@ -5,3 +5,36 @@
 // и реализовать функцию, которая меняет экстремальные элементы 
 // вектора местами. Если экстремальных элементов несколько, 
 // то необходимо поменять первые найденные элементы.
+
+#include "logic.h"
+
+int min(int array[DEFAULT_SIZE], int size) {
+	int min_value = 0;
+	for (int i = 0; i < size; i++)
+	{
+		if (array[i] < array[min_value]) {
+			min_value = i;
+		}
+	}
+	return min_value;
+}
+
+int max(int array[DEFAULT_SIZE], int size) {
+	int max_value = 0;
+	for (int i = 0; i < size; i++)
+	{
+		if (array[i] > array[max_value]) {
+			max_value = i;
+		}
+	}
+	return max_value;
+}
+
+void swap_extrem_elements(int array[DEFAULT_SIZE], int size) {
+	int max_index = max(array, size);
+	int min_index = min(array, size);
+
+	int temp = array[max_index];
+	array[max_index] = array[min_index];
+	array[min_index] = temp;
+}
--- Task04Project/logic.h
+++ Task04Project/logic.h
@@ -5,4 +5,8 @@
 
 using namespace std;
 
+int min(int array[DEFAULT_SIZE], int size);
+int max(int array[DEFAULT_SIZE], int size);
+
+
 void swap_extrem_elements(int array[DEFAULT_SIZE], int size);
